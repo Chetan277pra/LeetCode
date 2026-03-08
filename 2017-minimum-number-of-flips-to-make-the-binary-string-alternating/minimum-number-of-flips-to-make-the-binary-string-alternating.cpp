0aef17e8027a39f7bc5449310efb1b20511ad7d0
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-03-08 12:24:51 +0530

    Time: 15 ms (66.92%) | Memory: 17.1 MB (65.04%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
-    int minFlips(auto& s) {
-        int n = s.length();
-        int op[2] = {0, 0};
+    int minFlips(string s) {
 
-        for (int i = 0; i < n; i++)
-            op[(s[i] ^ i) & 1]++;
+        int n = s.size();
+        string t = s + s;
 
-        int res = min(op[0], op[1]);
+        int op0 = 0, op1 = 0;
 
-        for (int i = 0; i < n - 1; i++) {
-            op[(s[i] ^ i) & 1]--;
-            op[(s[i] ^ (n + i)) & 1]++;
-            res = min(res, min(op[0], op[1]));
+        for (int i = 0; i < n; i++) {
+            if (t[i] != (i % 2 ? '1' : '0'))
+                op0++;
+            if (t[i] != (i % 2 ? '0' : '1'))
+                op1++;
         }
 
-        return res;
+        int ans = min(op0, op1);
+
+        for (int i = n; i < 2 * n; i++) {
+
+            if (t[i - n] != ((i - n) % 2 ? '1' : '0'))
+                op0--;
+            if (t[i - n] != ((i - n) % 2 ? '0' : '1'))
+                op1--;
+
+            if (t[i] != (i % 2 ? '1' : '0'))
+                op0++;
+            if (t[i] != (i % 2 ? '0' : '1'))
+                op1++;
+
+            ans = min(ans, min(op0, op1));
+        }
+
+        return ans;
     }
 };
