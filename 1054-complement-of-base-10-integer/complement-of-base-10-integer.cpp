83913714760b56579dbfafd104b6f676ec18f6ae
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-03-11 17:25:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (4.02%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n == 0) return 1;
+        for(int i = 0; i <= log2(n); i++) 
+        n = n ^ (1<<i);
+        return n;
+    }
+};
