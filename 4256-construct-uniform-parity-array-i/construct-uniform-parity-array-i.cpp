075fd6ea99003532a6fcb07f6fd69407f2eb9fe5
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-03-22 13:05:51 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (9.15%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int odd = 0, even  = 0;
+        for(auto a : nums1) if(a%2) odd++; else even++;
+        bool evenans = true , oddans = true;
+        // even bna
+        for(auto a : nums1){
+            if(a%2 == 0 or odd > 1) continue;
+            evenans = false;
+            break;
+        }
+        // odd bna
+        for(auto a : nums1){
+            if(a%2 == 1 or odd > 0) continue;
+            oddans = false;
+            break;
+        }
+        return evenans or oddans;
+    }
+};
