c8e844ca491dee66c2037ca162373d717dd02448
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-04-15 01:54:09 +0530

    Time: 107 ms (30.40%) | Memory: 46.3 MB (39.86%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -1,33 +1,32 @@
+using ll = long long;
+ll n, m;
+vector<vector<ll>>dp;
 class Solution {
 public:
+    ll rec(vector<int>&rob, vector<int> &pos, int ind1, int ind2){
+        if(ind1 == n) return 0;
+        if(ind2 == m) return 1e14;
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        ll take = 0;
+        take = abs(rob[ind1] - pos[ind2]) + rec(rob, pos, ind1+1, ind2+1);
+        ll skip = rec(rob , pos , ind1, ind2+1);
+
+        return dp[ind1][ind2] = min(take , skip);
+    }
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
-        sort(robot.begin(), robot.end());
-        sort(factory.begin(), factory.end());
-
-        int n = robot.size();
-        int m = factory.size();
-
-        const long long INF = 1e18;
-        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, INF));
-
-        for (int j = 0; j <= m; j++)
-            dp[0][j] = 0;
-
-        for (int j = 1; j <= m; j++) {
-            int pos = factory[j-1][0];
-            int limit = factory[j-1][1];
-
-            for (int i = 0; i <= n; i++) {
-                dp[i][j] = dp[i][j-1]; // skip factory
-
-                long long dist = 0;
-                for (int k = 1; k <= limit && i - k >= 0; k++) {
-                    dist += abs(robot[i-k] - pos);
-                    dp[i][j] = min(dp[i][j], dp[i-k][j-1] + dist);
-                }
-            }
+        vector<int> pos;
+        sort(robot.begin() , robot.end());
+        sort(factory.begin() , factory.end());
+        n = robot.size();
+        ll factsize = factory.size();
+        for(int i = 0; i < factsize; i++){
+            ll aa = factory[i][1];
+            ll bb = factory[i][0];
+            for(ll j = 0; j < aa; j++) pos.push_back(bb);
         }
-
-        return dp[n][m];
+        m = pos.size();
+        dp.assign(n+1 , vector<ll>(m+1, -1));
+        // for(auto a: pos)cout<<a<<" ";
+        return rec(robot , pos, 0 , 0);
     }
 };
