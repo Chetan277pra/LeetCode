99fa055ab5cf4e6a6e3efa186209183d79ff8c46
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-04-22 23:03:49 +0530

    Time: 215 ms (6.82%) | Memory: 22.7 MB (6.53%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+
+    int rec(int pos , vector<int>& nums , vector<int> &dp){
+
+        int n = nums.size();
+        if(pos>=n-1) return 0;
+        if(dp[pos]!=1982) return dp[pos];
+        // if (nums[pos] == 0)
+        //     return INT_MAX;
+        int mini=INT_MAX;
+        for(int i=1 ; i<=nums[pos] ; i++){
+            if(pos+i< n ){
+            int result = rec(pos + i , nums , dp);
+            if(result!=INT_MAX)
+            mini = min(mini , 1+result);
+            }
+        }
+        return dp[pos]=mini;
+        
+    }
+public:
+    int jump(vector<int>& nums) {
+        int ans=0;
+        vector<int> dp(nums.size()+1 , 1982);
+        return rec(ans , nums , dp);
+    }
+};
