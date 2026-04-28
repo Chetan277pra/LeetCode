a8c6c265b0d3e3c70e7e8aa17edded1269d77ef3
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-04-28 19:57:18 +0530

    Time: 66 ms (6.17%) | Memory: 99.9 MB (5.35%) - LeetSync

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> vec;
+        for(auto a : grid){
+            for(auto b : a ) vec.push_back(b);
+        }
+        sort(vec.begin() , vec.end());
+        int si = vec.size();
+        int aa = vec[0]%x;
+        for(auto a : vec)
+        if(a%x != aa) return -1;
+        int mid = 0;
+        if(si%2){ 
+            mid = vec[si/2];
+            int ans = 0;
+            for(auto a : vec){
+                ans = ans + (abs(a - mid) + x - 1) / x ;
+            }
+            return ans;
+        }
+        else{
+            
+            mid = (vec[si/2 -1] + vec[si/2])/2;
+            int rem = mid%x;
+            cout<<rem<<endl;
+            int mid1 = vec[si/2 -1];
+            int mid2 = vec[si/2];
+            int ans1 = 0;
+            int ans2 = 0;
+            cout<<mid<<" "<<mid1<<" "<<mid2;
+            for(auto a : vec){
+            ans1 = ans1 + (abs(a - mid1) + x - 1) / x ;
+          }
+           for(auto a : vec){
+            ans2 = ans2 + (abs(a - mid2) + x - 1) / x ;
+          }
+
+          return min(ans1 , ans2);
+          
+        }
+         
+    }
+};
