4e8e20eb603623d70e04d804425fd1721acd3848
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-05-08 00:43:15 +0530

    Time: 14 ms (51.06%) | Memory: 225.7 MB (34.04%) - LeetSync

--- 3981-jump-game-ix/jump-game-ix.cpp
+++ 3981-jump-game-ix/jump-game-ix.cpp
@@ -2,37 +2,28 @@ class Solution {
 public:
     vector<int> maxValue(vector<int>& nums) {
         int n = nums.size();
-
-        vector<int> pre(n), suf(n), res(n);
-
-        // prefix max
-        pre[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            pre[i] = max(pre[i - 1], nums[i]);
+        
+        vector<int> suff(n);
+        vector<int> pref(n);
+        pref[0] = nums[0];
+        for(int i = 1; i < n; i++){
+        pref[i] = max(nums[i] , pref[i-1]);
+        }
+        suff[n-1] = nums[n-1];
+        for(int i = n-2; i >= 0; i--){
+        suff[i] = min(suff[i+1] , nums[i]);
         }
 
-        // suffix min
-        suf[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suf[i] = min(suf[i + 1], nums[i]);
+        // for(auto a : pref) cout<<a << " ";
+        // cout<<endl;
+        // for(auto a : suff) cout<<a<<" ";
+        // cout<<endl;
+        vector<int> ans(n);
+        ans[n-1] = pref[n-1];
+        for(int i = n-2; i >= 0; i--){
+            if(pref[i] > suff[i+1]) ans[i] = ans[i+1];
+            else ans[i] = pref[i];
         }
-
-        res[n - 1] = pre[n - 1];
-
-        // build answer
-        for (int i = n - 2; i >= 0; i--) {
-
-            // merge segment
-            if (pre[i] > suf[i + 1]) {
-                res[i] = res[i + 1];
-            }
-
-            // new segment
-            else {
-                res[i] = pre[i];
-            }
-        }
-
-        return res;
+        return ans;
     }
 };
