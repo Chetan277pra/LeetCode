a9831c00a4492d78d8df57708e1f0c6283e0271f
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-06-10 10:04:02 +0530

    Time: 265 ms (43.76%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,11 @@
+
+SELECT Department , Employee , Salary
+FROM (
+SELECT B.name as Department , A.name as Employee, A.Salary as Salary,
+DENSE_RANK() OVER(PARTITION BY A.departmentId ORDER BY A.Salary desc) AS DR
+FROM Employee as A
+JOIN Department as B  
+ON A.departmentId  = B.id
+) WHERE DR <= 3
+
+
