82106152739d696bcf430e7d4b52e0c9d3bab976
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-06-23 16:30:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.34%) - LeetSync

--- 461-hamming-distance/hamming-distance.cpp
+++ 461-hamming-distance/hamming-distance.cpp
@@ -3,9 +3,7 @@ public:
     int hammingDistance(int x, int y) {
         int ans = 0;
         for(int i = 0; i < 32; i++){
-            if( ((x & (1<<i)) and !(y&(1<<i)) )
-             or (!(x & (1<<i)) and (y&(1<<i)) )
-            ) ans++;
+            if ( (x & (1<<i)) != (y&(1<<i)) )  ans++;
         }
         return ans;
     }
