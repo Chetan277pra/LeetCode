75cc526136e4d58fb9aeb9fcbddcab218f1b8418
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-06-30 17:59:23 +0530

    Time: 1 ms (92.94%) | Memory: 10.9 MB (81.80%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,23 +1,42 @@
+// class Solution {
+// public:
+//     bool ok(vector<int> vec){
+//         return vec[0] > 0 and vec[1] > 0 and vec[2] > 0;
+//     }
+//     int numberOfSubstrings(string s) {
+//         int ans = 0;
+//         int n = s.length();
+//         int l = 0;
+//         vector<int> vec(26, 0);
+//         for(int i = 0; i < n; i++){
+//             vec[s[i] - 'a']++;
+//             if(ok(vec)){
+//                 int j = l;
+//                 while(j < i and ok(vec)) vec[s[j++] - 'a']--; 
+//                 ans += (j-l)*(n-i);
+//                 l = j;
+//             }
+//             // cout << i << "  " << ans << endl;
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
-    bool ok(vector<int> vec){
-        return vec[0] > 0 and vec[1] > 0 and vec[2] > 0;
-    }
     int numberOfSubstrings(string s) {
-        int ans = 0;
-        int n = s.length();
-        int l = 0;
-        vector<int> vec(26, 0);
-        for(int i = 0; i < n; i++){
-            vec[s[i] - 'a']++;
-            if(ok(vec)){
-                int j = l;
-                while(j < i and ok(vec)) vec[s[j++] - 'a']--; 
-                ans += (j-l)*(n-i);
-                l = j;
+        int last[3] = {-1, -1, -1}, left = 0, count = 0;
+        for(int right = 0; right < s.size(); right++){
+            char c = s[right];
+            last[c - 'a'] = right;
+            if(last[0] != -1 && last[1] != -1 && last[2] != -1){
+                count += min({last[0], last[1], last[2]}) + 1;
             }
-            // cout << i << "  " << ans << endl;
         }
-        return ans;
+
+        return count;
     }
 };
