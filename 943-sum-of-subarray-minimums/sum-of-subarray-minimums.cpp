d2071cf8718af5f6ed083f6d43d2beceec87655f
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-07-01 14:01:36 +0530

    Time: 48 ms (50.85%) | Memory: 193.5 MB (28.63%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -7,7 +7,7 @@ public:
         vector<int> nse(n) , pse(n);
         stack<int> st;
         for(int i = n-1; i >= 0; i--){
-            while(!st.empty() and arr[st.top()] >= arr[i])
+            while(!st.empty() and arr[st.top()] > arr[i])
             st.pop();
             if(st.empty()) nse[i] = n;
             else nse[i] = st.top();
@@ -15,7 +15,7 @@ public:
         }
         st = stack<int> {};
         for(int i = 0; i < n; i++){
-            while(!st.empty() and arr[st.top()]  > arr[i])
+            while(!st.empty() and arr[st.top()]  >= arr[i])
             st.pop();
             if(st.empty()) pse[i] = -1;
             else pse[i] = st.top();
