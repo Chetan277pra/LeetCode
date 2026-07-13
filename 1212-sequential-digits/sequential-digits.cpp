b7794db2ca3ea6f48b90020a0ef3c6076e73156a
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-07-13 21:57:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (76.91%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
-    inline static int q[45];
-    inline static bool init = []() {
-        int n = 0;
-        for (int i = 1; i < 10; i++)
-            q[n++] = i;
-
-        for (int i = 0; i < n; i++) {
-            int d = q[i] % 10;
-            if (d < 9) q[n++] = q[i] * 10 + d + 1;
+    int get(int n){
+        int count = 0;
+        while(n){
+            count++;
+            n /= 10;
         }
-
-        return 0;
-    }();
-
+        return count;
+    }
     vector<int> sequentialDigits(int low, int high) {
-        vector<int> res;
-
-        for (auto& x : q)
-            if (x >= low && x <= high)
-                res.push_back(x);
-
-        return res;
+        map<int , int> mpp;
+        mpp[2] = 12;
+        mpp[3] = 123;
+        mpp[4] = 1234;
+        mpp[5] = 12345;
+        mpp[6] = 123456;
+        mpp[7] = 1234567;
+        mpp[8] = 12345678;
+        mpp[9] = 123456789;
+        vector<int> one = {0 , 1 , 11 , 111 , 1111, 11111 , 111111, 1111111, 11111111, 111111111};
+        int l = get(low);
+        int r = get(high);
+        vector<int> ans;
+        for(int i = l; i < r; i++){
+            long long curr = mpp[i];
+            for(int j = 0; j < 10-i; j++){
+                // cout << curr <<"("<<j<<") ";
+                if(curr >= low) ans.push_back(curr); 
+                curr += one[i];
+            }
+            cout<<endl;
+        }
+        long long curr = mpp[r];
+        for(int i = 0; i < 10-r; i++){
+            if( curr >= low and curr <= high) ans.push_back(curr);
+            curr += one[r];
+        }
+        return ans;
     }
 };
