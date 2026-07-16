0be3f540d02f13f999fe2c2834020a5e203390cc
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-07-16 18:27:57 +0530

    Time: 76 ms (50.08%) | Memory: 148.8 MB (96.31%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     long long gcdSum(vector<int>& nums) {
-        multiset<int> pre;
         int maxi = 0;
-        for(auto a : nums){
-            if(a > maxi) maxi = a;
-            pre.insert(__gcd(a , maxi));
+        for(auto & a : nums){
+            maxi = max(a , maxi);
+            a = __gcd(a , maxi);
         }
-        long long ans = 0;
-        // for(auto a : pre) cout<<a <<" ";
-        int n = pre.size();
-        for(int i = 0; i < (n/2); i++){
-            int temp = __gcd(*pre.begin() , *prev(pre.end()) );
-            pre.erase(pre.begin());
-            pre.erase(prev(pre.end()));
-            ans += temp;
+        sort(nums.begin() , nums.end());
+        int l = 0 , r = nums.size()-1;
+        long long sum = 0;
+        while(l < r){
+            sum += __gcd(nums[l++] , nums[r--]);
         }
-        return ans;
+        return sum;
     }
 };
