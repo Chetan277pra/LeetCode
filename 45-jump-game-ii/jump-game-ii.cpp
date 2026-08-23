776156301057425bf6eef775e66d4ff6eeff9c4d
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-08-23 16:39:20 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (54.28%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,27 +1,25 @@
 class Solution {
-
-    int rec(int pos , vector<int>& nums , vector<int> &dp){
-
-        int n = nums.size();
-        if(pos>=n-1) return 0;
-        if(dp[pos]!=82) return dp[pos];
-        // if (nums[pos] == 0)
-        //     return INT_MAX;
-        int mini=INT_MAX;
-        for(int i=1 ; i<=nums[pos] ; i++){
-            if(pos+i< n ){
-            int result = rec(pos + i , nums , dp);
-            if(result!=INT_MAX)
-            mini = min(mini , 1+result);
-            }
-        }
-        return dp[pos]=mini;
-        
-    }
 public:
     int jump(vector<int>& nums) {
-        int ans=0;
-        vector<int> dp(nums.size()+1 , 82);
-        return rec(ans , nums , dp);
+        int n = nums.size();
+        if(n == 1) return 0;
+        int ans = 1;
+        int i = 0;
+        int maxi = nums[0];
+        while(i <= maxi){
+            int temp = i+1;
+            if(maxi >= n-1) return ans;
+            for(int j = i+1; j <= i+nums[i] and j < n; j++){
+                if(j + nums[j] > maxi){
+                    temp = j;
+                    maxi = j + nums[j];
+                }
+            }
+            ans++;
+            i = temp;
+            // cout<<i<<" "<<ans<<" " <<maxi<<endl;
+            if(maxi >= n-1) return ans;
+        }
+        return -1;
     }
 };
