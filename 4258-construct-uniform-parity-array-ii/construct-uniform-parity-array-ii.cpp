9581d9bd68f99f564d805d9eb01affc3d88d2d20
Chetan prakash <159622634+Chetan277pra@users.noreply.github.com>
2026-09-03 23:08:05 +0530

    Time: 0 ms (100.00%) | Memory: 165.9 MB (62.83%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
-    bool uniformArray(vector<int>& nums1) {
-        int mini = INT_MAX;
-        int odd = 0;
-        for(auto a : nums1) if(a%2) {mini = min(mini , a); odd++;}
-        // even bna
-        bool evenans = true,  oddans = true;
-        for(auto a : nums1){
-            if(a%2 ==0 or (odd > 1 and a-1 >= mini) ) continue;
-            evenans = false;
-            break;
+    bool uniformArray(vector<int>& nums) {
+        int odd = 0 , even = 0 , mini = INT_MAX;
+        for(auto a : nums){
+            odd += (a%2 == 1);
+            even += (a%2 == 0);
+            mini = min(mini , a);
         }
-        // odd bna
-        for(auto a : nums1){
-            // cout<<"odd ans"<<oddans<<" a : "<<a<<endl;
-            if(a%2 == 1 or (odd > 0 and a-1 >= mini)) continue;
-            oddans = false;
-            break;
-        }
-        return oddans or evenans;
+        if(mini%2 == 0 and odd > 0) return false;
+        // if(mini%2 == 1 and even > 0) return false;
+        return true;
     }
 };
